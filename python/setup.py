eb2853cc2f10d88366b8ec5e0acf1b5806391f88
Andreas Blech <blech@permarobotics.com>
2023-12-12 08:55:32 +0100

    python: updated dependency to openral core

--- python/setup.py
+++ python/setup.py
@@ -1,31 +1,32 @@
-
 from setuptools import find_packages, setup
 
-#run:
-#python3 setup.py sdist bdist_wheel
+# run:
+# python3 setup.py sdist bdist_wheel
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
 setup(
-    name='openral_mongodb_py',
-    version='0.1.15',
-    description = 'MongoDB Implementation of a RalRepositories for openRAL.',
+    name="openral_mongodb_py",
+    version="0.1.16",
+    description="MongoDB Implementation of a RalRepositories for openRAL.",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    author='Permarobotics GmbH',
-    author_email='blech@permarobotics.com',
-    url='https://github.com/AndyPermaRobotics/openral-mongodb-py',
-    packages=find_packages(include=['openral_mongodb_py', 'openral_mongodb_py.*', 'openral_mongodb_py.*.*']),
-    license='MIT',
+    long_description_content_type="text/markdown",
+    author="Permarobotics GmbH",
+    author_email="blech@permarobotics.com",
+    url="https://github.com/AndyPermaRobotics/openral-mongodb-py",
+    packages=find_packages(
+        include=["openral_mongodb_py", "openral_mongodb_py.*", "openral_mongodb_py.*.*"]
+    ),
+    license="MIT",
     install_requires=requirements,
     keywords=[
-        'openRAL', 
-        'regenerative agriculture', 
-        'agriculture',
-        'mongodb',
+        "openRAL",
+        "regenerative agriculture",
+        "agriculture",
+        "mongodb",
     ],
 )
